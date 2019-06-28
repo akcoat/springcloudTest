e72058c436a547de780c8d921f422a411d10f399
akcoat <akcoat@qq.com>
2019-06-28 16:35:59 +0800

    解决了config server  因注册中心引入安全验证 无法注册的问题

---
+++ paascloud-eureka/src/main/java/com/paascloud/discovery/eureka/config/webConfig.java
@@ -0,0 +1,17 @@
+package com.paascloud.discovery.eureka.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class webConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+    }
+
+}
